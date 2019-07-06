a936b604a1d17c57140b6b5faaa85ea94ac98ada
Trung Dang <trungdt@absoft.vn>
2019-07-07 06:39:28 +0800

    feat: add swagger and global validation

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,16 +1,19 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { GlobalModule } from './global.module';
 import { MwGracefulShutdown, MwRequestLogger } from '../middlewares';
-import { providerErrorFilter } from '../providers';
+import { providerErrorFilter, providerGlobalValidation } from '../providers';
+import UserController from '../controllers/user.controller';
 
 @Module({
   imports: [
     GlobalModule.forRoot()
   ],
   controllers: [
+    UserController
   ],
   providers: [
-    providerErrorFilter
+    providerErrorFilter,
+    providerGlobalValidation
   ]
 })
 export class AppModule implements NestModule {
